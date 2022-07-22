53ee1b1cb16ce3037e00605079ddc5ac76581526
dmilanop <dmilanop28@gmail.com>
2022-07-22 16:33:17 +0000

    Voy en el 5.1

--- exercises/04.1-count_on/app.py
+++ exercises/04.1-count_on/app.py
@@ -3,6 +3,11 @@ my_list = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
 #your code go here:
 new_list = []
 
-for i in range(len(my_list)):
-    new_list.append(my_list[i])
-    print(new_list)
+for x in range(0, len(my_list)):
+    if type(my_list[x]) == dict:
+        new_list.append(my_list[x])
+        print(new_list)
+    elif type(my_list[x]) == list: 
+        new_list.append(my_list[x])
+        print(new_list)
+    # print(type(new_list[x]))
