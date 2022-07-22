a19ce36387927ca2a367a3e789270acf2796d85b
dmilanop <dmilanop28@gmail.com>
2022-07-22 02:26:49 +0000

    Trabjando el 4.1

--- exercises/02.4-One_last_looping/app.py
+++ exercises/02.4-One_last_looping/app.py
@@ -1,3 +1,12 @@
-names = ['Esmeralda','Kiko','Ruth','Lebron','Pedro','Maria','Lou','Fernando','Cesco','Bart','Annie']
+names = ['Ruth'+'Pedro','Steven','Ruth','Lebron','Pedro','Maria','Lou','Fernando','Cesco','Bart','Pepe']
 
 #Your code here:
+# del names[1]
+# names.insert(1, "Steven")
+# del names[10]
+# names.insert(10, "Pepe")
+# del names[0]
+# names.insert(0, "Ruth"+"Pedro")
+
+for i in range(len(names)-1, -1, -1):
+    print(names[i])
--- exercises/04.1-count_on/app.py
+++ exercises/04.1-count_on/app.py
@@ -1,3 +1,8 @@
 my_list = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
 
 #your code go here:
+new_list = []
+
+for i in range(len(my_list)):
+    new_list.append(my_list[i])
+    print(new_list)
