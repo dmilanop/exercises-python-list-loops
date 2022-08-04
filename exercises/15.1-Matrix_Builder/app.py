40308ebfbcb0ff32b8cfa27534aa00d937ec5b9e
dmilanop <dmilanop28@gmail.com>
2022-08-04 21:09:56 +0000

    Terminado :)

--- exercises/15.1-Matrix_Builder/app.py
+++ exercises/15.1-Matrix_Builder/app.py
@@ -1,3 +1,11 @@
 #Import random
-
+import random
 #Create the function below:
+def matrixBuilder(number):
+    list_matrix = [ 
+        [1, 1, 1]
+    ]
+    return number * list_matrix
+
+
+print(matrixBuilder(3))
